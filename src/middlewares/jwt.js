c5829c8e80ce1a9b91a4830c9fd9afed6e57d2a0
Luis Vera <luisvera2318@gmail.com>
2023-01-18 09:05:45 -0600

    fix generate token

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
 
 const generateJWT = (id, name) => {
-	return new Promise((resolve, reject) => {
+	return new Promise(async (resolve, reject) => {
 		const payload = { id, name };
 
-		jwt.sign(
-			payload,
-			process.env.SECRET_JWT_SEED,
-			{
-				expiresIn: '2h',
-			},
-			(err, token) => {
-				if (err) {
-					console.log(err);
-					reject('No se pudo generar el token');
+		try {
+			await jwt.sign(
+				payload,
+				process.env.SECRET_JWT_SEED,
+				{
+					expiresIn: '2h',
+				},
+				(err, token) => {
+					if (err) {
+						console.log(err);
+						reject('No se pudo generar el token');
+					}
+					resolve(token);
 				}
-				resolve(token);
-			}
-		);
+			);
+		} catch (err) {
+			console.error('error al generar el token');
+			console.error(err);
+		}
 	});
 };
 
